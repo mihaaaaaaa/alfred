3ab1156b273b3cb9ec2e47a09f0c4b5401484357
bananaWeb <a89.volkov@gmail.com>
2018-02-01 19:01:01 +0300

    add logic

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 $(document).ready(function (){
-	var alfredspeech ='привет,гуманоиды!';
-	$('.js-showhide').click(function() {
-		$('.alfred').toggleClass('-invisible');
+	$('.js-count').click(function() {
+		showBuble($('.js-firstNumber').val() + $('.js-secondNumber').val());
 	});
-	$('.js-alfredspeech').click(function(){
-		$('.bubble').text(alfredspeech);
-	});
+
+	function showFirstInput () {
+		var inputText = $('input').val();
+		if (inputText === "") {
+			showBuble("Да тут же пусто !!!");
+		} else {
+			showBuble(inputText);
+		}
+	}
+	function showBuble(Text) {
+		$('.bubble').text(Text);
+		$('.bubble').addClass('-visible');
+		setTimeout(function() { 
+			$('.bubble').removeClass('-visible');
+		}, 4000);
+	}
 });
